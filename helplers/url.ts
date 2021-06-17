d7941cdef2ba041a087f98d0cc84e14163d79b4f
chencf10 <chencf10@meicloud.com>
2021-06-17 18:01:38 +0800

    feat(exapmles/base): 处理请求url参数

---
+++ helplers/url.ts
@@ -0,0 +1,77 @@
+import { isDate, isObject } from "./util";
+
+/**
+ * 
+ * @param url 
+ * @param params 
+ * @description  处理url参数
+ * 
+ */
+export function buildURL(url: string, params?: any): string {
+    if (!params) {
+        return url;
+    }
+
+    const parts:string[] = [];
+    // 循环处理参数
+    Object.keys(params).forEach(key => {
+        let val = params[key];
+        // 清除为null undefined 参数
+        if (val === null || val === undefined) {
+            return ;
+        }
+        // 数组的值，拼上key[]
+        let values:string[] = [];
+        if (Array.isArray(val)) {
+            values = val;
+            key += '[]';
+        } else {
+            values = [val];
+        }
+
+        // 拼接参数
+        values.forEach(item => {
+            if(isDate(item)) {
+                item = item.toISOString();
+            }
+
+            if(isObject(item)) {
+                item = JSON.stringify(item);
+            }
+
+            parts.push(`${encode(key)}=${encode(item)}`);
+        })
+
+    })
+    let serializedParams = parts.join('&');
+    // 清除hash #
+    const markIndex = url.indexOf('#');
+    if (markIndex !== -1 ) {
+        url = url.slice(0, markIndex);
+    }
+    if (serializedParams) {
+        // 判断url 末尾是否有?
+        url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+    }
+
+    return url;
+}
+
+
+
+/**
+ * 
+ * @param val 
+ * @description 对于字符 @、:、$、,、、[、]，我们是允许出现在 url 中的，不希望被 encode。
+ *  
+ */
+function encode(val: string): string {
+    return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']');
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import { AxiosRequestConfig } from './types'
+import { buildURL } from '../helplers/url'
 import xhr from './xhr'
 
 function axios(config: AxiosRequestConfig): void {
+  processConfing(config)
   xhr(config)
+}
+
+function processConfing(config: AxiosRequestConfig): void {
+  config.url = transformConfig(config)
+}
+function transformConfig(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default axios
