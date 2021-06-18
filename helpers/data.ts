f3bc6a8716154193fe804d4211458f964f1466b6
chencf10 <chencf10@meicloud.com>
2021-06-18 11:22:59 +0800

    feat(helper/data.ts): data是不能直接传给 send 函数的，我们需要把它转换成 JSON 字符串。
    #

---
+++ helpers/data.ts
@@ -0,0 +1,9 @@
+import { isPlainObject } from './util';
+
+export function transformRequest(data: any): any {
+    if (isPlainObject(data)) {
+        data = JSON.stringify(data);
+    }
+
+    return data;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { AxiosRequestConfig } from './types'
-import { buildURL } from '../helplers/url'
+import { buildURL } from '../helpers/url'
+import { transformRequest } from '../helpers/data'
 import xhr from './xhr'
 
 function axios(config: AxiosRequestConfig): void {
@@ -9,10 +10,14 @@ function axios(config: AxiosRequestConfig): void {
 
 function processConfing(config: AxiosRequestConfig): void {
   config.url = transformConfig(config)
+  config.data = transformRequestData(config)
 }
 function transformConfig(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+function transformRequestData(config: AxiosRequestConfig): string {
+  return transformRequest(config.data)
 }
 
 export default axios
